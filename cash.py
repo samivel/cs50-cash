1b510df93376136bd2ee9d7b37f47b982c72e6bf
Samivel Evans <samivel@gmail.com>
2020-07-29 17:20:51 -0400

    Initial commit

---
+++ cash.py
@@ -0,0 +1,25 @@
+from cs50 import get_float
+
+def main():
+    while True:
+        change = get_float("Change owed: ")
+        if change > 0:
+            break
+
+    centsOwed = int(change * 100)
+    print(cash(centsOwed))
+
+
+def cash(n):
+    quarters = n / 25
+    dimes = (n % 25) / 10
+    nickels = ((n % 25) % 10) / 5
+    pennies = ((n % 25) % 10) % 5
+
+    print(f"Quarters: {int(quarters)}")
+    print(f"Dimes: {int(dimes)}")
+    print(f"Nickels: {int(nickels)}")
+    print(f"Pennies: {int(pennies)}")
+    return int(quarters) + int(dimes) + int(nickels) + int(pennies)
+
+main()
